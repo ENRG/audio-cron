702a5b5f9e507f724bbcee5f7f6443548286afbc
John Fawcett <jrf0110@gmail.com>
2014-02-22 13:47:12 -0600

    complete options for arecord

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var moment  = require('moment');
 var config = {
   period:     '00 * * * * *'
 , immediate:  true
-, arecord:    [ '-d', 60, '-r', 64 ]
+, arecord:    [ '-f', 'cd', '-D', 'plughw:1,0', '-d', 60, '-r', 64 ]
 , lame:       [ '-x', '-r', '-' ]
 , wavDir:     '../ftp/wav'
 , mp3Dir:     '../ftp/mp3'
